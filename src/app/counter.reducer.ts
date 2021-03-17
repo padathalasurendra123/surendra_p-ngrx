cf93ddf1b2ede4d06512ae37f61c5c24be2c8d14
padathalasurendra789 <padathalasurendra@gmail.com>
2021-03-17 06:25:20 +0530

    changes for sample actions

--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -1,3 +1,6 @@
+import { createReducer, on } from "@ngrx/store";
+import { decrement, increment, reset } from "./counter.actions";
+
 export const initialState = 0;
  
 const _counterReducer = createReducer(
